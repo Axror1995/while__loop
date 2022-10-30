8a310eac866c2537eba9e202cbe9d186290357e6
aror raximmov <axrorraximov108@gmail.com>
2022-10-30 22:09:05 +0500

    03

--- bin/while__loop03.dart
+++ bin/while__loop03.dart
@@ -1,3 +1,10 @@
 void main() {
-  //Print your name 2343 times
+  int b = 0;
+  while (b < 2343) {
+    
+    b+= 1;
+    print('axror'+b.toString());
+    
+  }
+  //Print your name a hundred times
 }
