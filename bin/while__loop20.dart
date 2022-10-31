ffa71f732f41b21ff27ae1580862d74e0a96f0a8
aror raximmov <axrorraximov108@gmail.com>
2022-10-31 12:23:59 +0500

    20

--- bin/while__loop20.dart
+++ bin/while__loop20.dart
@@ -1,10 +1,10 @@
 void main() {
   int a = 12;
-  int b = 1;
+  int c = 1;
   while (a <= 34) {
     a++;
-    b *= a;
+    c *= a;
   }
-  print(b);
+  print(c);
   //Find the product of the numbers from 12 to 34
 }
